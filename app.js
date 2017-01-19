4ed379371fba0bc642eef0cfe30dcfc2047d48e0
Kasra <kasra@codeforaustralia.org>
2017-01-19 17:58:12 +1100

    initialization - node js packages installed

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+var http = require('http');
 
-  console.log("Allah will do it for do! You just try your best!");
+
+var server = http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('You will do it by God`s help');
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('server is listening');
